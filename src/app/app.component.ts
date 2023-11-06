dd8122df99c18c6d8d36ae5fd7687d06600c48b7
apurba-striker <apurbapatra1234@gmail.com>
2023-11-06 17:39:54 +0530

    done work

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,22 @@
-import { Component, OnInit } from '@angular/core';
-import { ApiService } from './services/api.service';
+import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { AboutAppComponent } from './shared/components/about-app/about-app.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit{
-  constructor(
-    private apiService: ApiService
-  ) {}
+export class AppComponent {
+  title = 'JMA-test';
 
-  ngOnInit() {
-    this.apiService.getUser('johnpapa').subscribe(console.log);
+  constructor(public dialog: MatDialog) {}
+
+  openDialog() {
+    const dialogRef = this.dialog.open(AboutAppComponent);
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log(`Dialog result: ${result}`);
+    });
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,13 @@
-import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-import { AppComponent } from './app.component';
 import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { SearchModule } from './search/search.module';
+import { SharedModule } from './shared/shared.module';
+import { MaterialModule } from './shared/material/material.module';
 
 @NgModule({
   declarations: [
@@ -9,7 +15,13 @@ import { BrowserModule } from '@angular/platform-browser';
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    MaterialModule,
+    SharedModule,
+
+    SearchModule
   ],
   providers: [],
   bootstrap: [AppComponent]
